8fce52e974c227af7872fc35f4b4c992088250cd
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-12-02 04:30:50 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,8 +13,9 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  binding.pry
+  
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
   binding.pry
+  end
 end
